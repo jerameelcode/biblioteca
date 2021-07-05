9e54acaa3909a6ab89a125dc5db3bd9110dfbc22
jerameelcode <jerameelcode@gmail.com>
2021-07-05 19:06:15 -0300

    [feature] Spring security to find users credentials from database.

--- src/main/java/pt/adentis/biblioteca/config/SecurityConfig.java
+++ src/main/java/pt/adentis/biblioteca/config/SecurityConfig.java
@@ -1,13 +1,46 @@
 package pt.adentis.biblioteca.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @EnableAuthorizationServer
 @EnableResourceServer
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity)
+            throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+        httpSecurity.csrf()
+                .ignoringAntMatchers("/h2-console/**");
+        httpSecurity.headers()
+                .frameOptions()
+                .sameOrigin();
+    }
 
 }
